23a83a49b96dd3292ca1efb658da611715b1e06c
Mustafa-Arshad <115984208+Mustafa-Arshad@users.noreply.github.com>
2022-10-30 02:26:55 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+alert("1st class java script");
+alert("Hello js")
+
+let A = 10;
+let B = 10;
+console.log(a+b)
+
+const input = prompt("your name");
+alert('Your name ${input}');
+alert("thanks for starting java script");
+
+var b = 20;
+
+alert(20)
+
+var a = 20;
+var b = 30;
+
+var c = (a+b) * a;
+alert(c)
+
+var a;
+
+a = "Mohammad Mustafa";
+alert(a);
+
+a = "ABC"
+alert(a);
+
+var a = 20;
+var b = "30";
+
+var c = b + a;
+
+
+alert(c);
+
+var abc //legal
+var 123 //illegal
+var 12ab //illegal
+var ab12 //legal
+var _  //legal
+var $ //legal
+var alert //illegal
+
+var a = 20
+var b = 30
+
+a++;
+b--;
+
+console.log(a);
+console.log(b)
+
+var a = 20;
+
+
+var b = a++;
+
+console.log(b);
+console.log(b);
