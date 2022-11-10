3da0d10a468e2495394db07e27f81df9dc8b34ba
Mustafa-Arshad <115984208+Mustafa-Arshad@users.noreply.github.com>
2022-11-10 00:21:28 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,52 +1,27 @@
-// var subject1 = +prompt("physics Marks /100")
-// var subject2 = +prompt("Maths Marks /100")
-// var subject3 = +prompt("Urdu Marks /100")
-// var subject4 = +prompt("Computer Marks /100")
-// var subject5 = +prompt("English Marks /100")
+var a = ["a" , "b" , "c" , "d" , "e" ,]
 
-// console.log("Physics: ", +subject1);
-// console.log("Maths: ", +subject2);
-// console.log("Urdu: ", +subject3);
-// console.log("Computer: ", +subject4);
-// console.log("English: ", +subject5);
-// var total = subject1 + subject2 + subject3 + subject4 + subject5
-// console.log("Total Marks: 500")
-// console.log("Total Obtained Marks", +total)
-// var percentage = total / 500 * 100
-// console.log("percentage: ", percentage + "%");
+//push
+//pop
+//shift
+//unshift
+//slice
+//splice
 
-// if (percentage >= 90 && percentage <= 100) {
-//     console.log("Congrats! you've Secured A++")
-// }
-// else if (percentage >= 80 && percentage <= 90) {
-//     console.log("Congrats! you've Secured A+")
-// }
-// else if (percentage >= 70 && percentage <= 80) {
-//     console.log("Congrats! you've Secured A")
-// }
-// else if (percentage >= 60 && percentage <= 70) {
-//     console.log("Congrats! you've Secured B Grade")
-// }
-// else if (percentage >= 50 && percentage <= 60) {
-//     console.log("Congrats! you've Secured C Grade")
-// }
-// else if (percentage >= 40 && percentage <= 50) {
-//     console.log("Congrats! you've Secured D Grade")
-// }
-// else {
-//     console.log("you've Failed")
-// }
+// a.push("L");
+// a.pop();
+// a.shift();
+// a.unshift("x");
+// var b = a.slice(0,2)
+// var b = a.splice(2,3)
+// a.splice(2,1)
+// a.splice(2,0, "x", "j", "w", "d", "f", "g", "c", "z", )
+
+//console.log(a);
+//console.log(b);
+// var i = a.indexOf("e")
+// console.log(a[i]);
 
 
-
-// var a = prompt("what is your name")
-
-// var b = prompt("Where does the Pope live?");
-
-// var cell0 = "samsung";
-// var cell1 = "Iphone";
-// var cell2 = "realme";
-// var cell3 = "oppo";
-// var cell4 = "vivo";
-// var cell5 = "techno";
-// alert("Welcome to " + cell4);
+// for(var i=1; i < 10; i++){
+//     console.log('4x', +i+'=', 4*1)
+// }
