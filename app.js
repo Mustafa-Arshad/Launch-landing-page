1ba3c5977de1364a79ac060005462bdc1743b8aa
Mustafa-Arshad <115984208+Mustafa-Arshad@users.noreply.github.com>
2022-12-15 16:54:20 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,23 +1,148 @@
-//   ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ Topic: Changing Case ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬//
-// Task : 1.
-// var a = prompt("Enter Any Word, We Convert it into UPPERCASE");
-// var b = a.toUpperCase();
-// document.write(b);
 
-// Task : 2.
-// var input = prompt("Enter Any Word, We Convert it into TitleCase")
-// var firstLetter = input.slice(0, 1);
-// var otherLetters = input.slice(1);
-// input = firstLetter.toUpperCase() + otherLetters.toLowerCase();
-// document.write(input);
+var arry = [
+    {
+        rollNum: 'sm002',
+        name: "Muhammad rafay",
+        course: "Web and mobile app",
+        age: "18"
 
-//   ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ Topic: Strings: measuring length and extracting parts ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬//
-// Task : 1.
-// var mobile = prompt("Enter Your Favorite Mobile Phone Model");
-// var length = mobile.length;
-// document.write(length);
 
-// Task : 2.
-// var str1 = prompt("Enter Your Favorite Mobile Phone Model");
-// var str2 = str1.charAt(str1.length-1);
-// document.write(str2)
+
+
+    },
+    {
+        rollNum: 'sm001',
+        name: "Muhammad Mustafa",
+        course: "Web and mobile app",
+        age: "18"
+
+
+
+
+    },
+    {
+        rollNum: 'sm003',
+        name: "Muhammad ahmed",
+        course: "Web and mobile app",
+        age: "18"
+
+
+
+
+    },
+    {
+        rollNum: 'sm004',
+        name: "Muhammad basit",
+        course: "Web and mobile app",
+        age: "18"
+
+
+
+
+    },
+    {
+        rollNum: 'sm005',
+        name: "Muhammad saud",
+        course: "Web and mobile app",
+        age: "18"
+
+
+
+
+    },
+    {
+        rollNum: 'sm006',
+        name: "Muhammad bilal",
+        course: "Web and mobile app",
+        age: "18"
+
+
+
+
+    },
+    {
+        rollNum: 'sm008',
+        name: "Muhammad Ali",
+        course: "Web and mobile app",
+        age: "18"
+
+
+
+
+
+    },
+    {
+        rollNum: 'sm010',
+        name: "Muhammad hassan",
+        course: "Web and mobile app",
+        age: "18"
+    }
+
+];
+// console.log(arry)
+// console.log(student1)
+
+// var student2 = {
+//     rollNum: 'sm002',
+//     name: "Muhammad rafay",
+//     course: "Web and mobile app",
+//     age: "18"
+
+
+
+
+// };
+// console.log(student2)
+
+// var student3 = {
+//     rollNum: 'sm003',
+//     name: "Muhammad ahmed",
+//     course: "Web and mobile app",
+//     age: "18"
+
+
+
+
+// };
+// console.log(student3)
+
+
+// console.log(student4)
+
+// var student5 = ;
+// console.log(student5)
+
+// var student6 = ;
+// console.log(student6)
+// var student7 = {
+//     rollNum: 'sm007',
+//     name: "Muhammad rasheed",
+//     course: "Web and mobile app",
+//     age: "18"
+
+
+
+
+// };
+
+// console.log(student7);
+// var student8 = ;
+
+// console.log(student8);
+// var student9 = ;
+
+// console.log(student9);
+// var student10 = ;
+
+// console.log(student10);
+
+var a = document.getElementById("inp")
+function search() {
+    // console.log(a)
+    for (var i = 0; i < arry.length; i++) {
+        if (a.value == arry[i].rollNum) {
+            // console.log("Found");
+            console.log(arry[i]);
+        }
+    }
+}
